4d041e3ec58704893ad63aa1acc6fd3d02cb068a
Paulo Cézar Júnior <paulocezar.francisco.junior@gmail.com>
2018-02-25 14:09:55 -0300

    Building Hero Page | Update service worker

--- serviceworker.js
+++ serviceworker.js
@@ -11,7 +11,7 @@
  limitations under the License.
 */
 
-var CACHE_VERSION = 12;
+var CACHE_VERSION = 13;
 var CURRENT_CACHES = {
     heroes: 'heroes-cache-v' + CACHE_VERSION
 };
@@ -19,7 +19,9 @@ var CURRENT_CACHES = {
 self.addEventListener('install', function(event) {
     var urlsToPrefetch = [
         'css/mobile/app.css',
+        'css/mobile/detail.css',
         'css/all/app.css',
+        'css/all/detail.css',
         'index.html',
         'js/util.js',
         'js/listeners.js',

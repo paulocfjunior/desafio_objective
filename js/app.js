9fc8a6f3d4f6f105959fd76bd1cb536aaaad119a
Paulo Cézar Júnior <paulocezar.francisco.junior@gmail.com>
2018-02-25 11:42:15 -0300

    CSS Mobile Release

--- js/app.js
+++ js/app.js
@@ -363,7 +363,7 @@ function setPage(p) {
             get("app-pagination-next").removeClass("disabled");
         }
 
-        if((pages.length > 4) && (p > 2) && (p < pages.length - 1)) {
+        if((pages.length > 4) && (p > 1) && (p < pages.length - 1)) {
             pages.removeClass("mobile-visible");
             for(let i = 0; i < pages.length; i++){
                 if((i >= (p - 2)) && (i <= (p + 2))){
